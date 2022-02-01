f22f8ab4ffb22baf5c41279cd138cc0af57d4250
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-02-01 11:38:24 -0700

    updating fetch according to video

--- src/Login.js
+++ src/Login.js
@@ -1,31 +1,59 @@
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 
 function Login({ logUserIn }) {
   const [accountExists, setAccountExists] = useState(true);
-  console.log(accountExists)
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [loginUsername, setLoginUsername] = useState('');
+  const [loginPassword, setLoginPassword] = useState('');
+
+  const [loggedInUsername, setLoggedInUsername] = useState('')
 
   function logUserIn(e) {
     e.preventDefault();
     console.log("login fetch goes here");
-    const usernameInput = e.target.username_field.value;
-    const passwordInput = e.target.password_field.value;
-    console.log(usernameInput)
-    console.log(passwordInput)
-  }
+    const loginData = {
+        user: {username: loginUsername, password: loginPassword}
+    }
+
+    fetch(`http://localhost:3000/api/v1/login`, {
+        method: 'POST', 
+        headers: {
+            accepts: 'application/json',
+            'Content-type':'application/json',
+        },
+        body: JSON.stringify(loginData),
+    })
+    .then((res) => res.json())
+    .then((json) => localStorage.setItem('jwt', json.jwt));
+    setLoginUsername('');
+    setLoginPassword('');
+    }
+
+
 
   function hideLogin(e) {
-      e.preventDefault();
-      setAccountExists(false)
+    e.preventDefault();
+    setAccountExists(false);
   }
 
   function createProfile(e) {
-      e.preventDefault();
-      console.log("create account fetch goes here")
-      const chosenUsername = e.target.create_username.value;
-      const chosenPassword = e.target.create_password.value;
-      console.log(chosenUsername)
-      console.log(chosenPassword)
+    e.preventDefault();
+
+    fetch(`http://localhost:3000/api/v1/users`, {
+      method: "POST",
+      headers: {
+        Accepts: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ user: { username, password } }),
+    })
+      .then((res) => res.json())
+      .then((json) => console.log("backend connection", json));
+      setUsername('')
+      setPassword('')
   }
 
   return (
@@ -33,18 +61,38 @@ function Login({ logUserIn }) {
       {accountExists ? (
         <div>
           <form className="login" onSubmit={logUserIn}>
-            <input type="text" name="username_field" placeholder="Username" />
-            <input type="text" name="password_field" placeholder="Password" />
-            <button type="submit" >Login</button>
+            Username:{" "}
+            <input
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              placeholder="Username"
+            />
+            Password:{" "}
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder="Password"
+            />
+            <button type="submit">Login</button>
           </form>
           <button onClick={hideLogin}>Need an account?</button>
         </div>
       ) : (
         <form className="create_profile" onSubmit={createProfile}>
-            <input type="text" name="create_username" placeholder="Create Username" />
-            <input type="text" name="create_password" placeholder="Create Password" />
-            <button type="submit" >Create Profile</button>
-          </form>
+          <input
+            type="text"
+            name="create_username"
+            placeholder="Create Username"
+          />
+          <input
+            type="text"
+            name="create_password"
+            placeholder="Create Password"
+          />
+          <button type="submit">Create Profile</button>
+        </form>
       )}
     </div>
   );
