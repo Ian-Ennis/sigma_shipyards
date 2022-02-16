b3233c027803e36487f11ed81f3a9839631d3523
duneSpice97 <89211252+duneSpice97@users.noreply.github.com>
2022-02-16 16:59:03 -0700

    removed jwt from getShips

--- src/features/systems/SystemsSlice.js
+++ src/features/systems/SystemsSlice.js
@@ -4,12 +4,12 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 export const fetchSystems = createAsyncThunk("systems/fetchSystems", async () => { /*async functions usually return promises*/
   console.log('in systems fetch')
   
-  const response = await fetch(`http://localhost:3000/star_systems`, {
+  const response = await fetch(`https://sigma-shipyards-backend.herokuapp.com/star_systems`, {
     method: "GET",
     headers: {
       Accepts: "application/json",
       "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("jwt")}`,
+      // Authorization: `Bearer ${localStorage.getItem("jwt")}`,
     },
   })
   .then((res) => res.json())
